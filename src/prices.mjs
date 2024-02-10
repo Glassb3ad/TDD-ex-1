768c2247aac0338f3c3a8daffffd4b7d0490ff31
GlassB3ad <j.lahtivirta@gmail.com>
2024-02-10 18:29:57 +0200

    convert function

--- src/prices.mjs
+++ src/prices.mjs
@@ -4,6 +4,7 @@ import express from "express";
 // Refactor the following code to get rid of the legacy Date class.
 // Use Temporal.PlainDate instead. See /test/date_conversion.spec.mjs for examples.
 
+const convert = (date) => new Temporal.PlainDate(date.getFullYear(), date.getMonth(), date.getDate())
 function createApp(database) {
   const app = express();
 
